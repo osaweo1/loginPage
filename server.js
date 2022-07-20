9544abef177695ed00bb25faf03c19ebf7c8bc10
osamudiamen osawe <osamudiamenosawe@gmail.com>
2022-07-20 14:18:14 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express')
+
+const app=express()
+app.use(express.static('public'))
+
+
+
+
+const port=5000;
+app.listen(port,()=>{
+    console.log(`Server is listening on http://localhost:${port}`)
+})
